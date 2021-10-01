c277020bd76dc989e9d8642128e1c029e6217ec1
Estanislao <emarinwalker@uca.edu.ar>
2021-09-30 22:15:13 -0300

    Más migrations con datos cargados por default. Dashboard de gráficos.

--- database/migrations/2021_09_07_214800_create_eventos_table.php
+++ database/migrations/2021_09_07_214800_create_eventos_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Evento;
+
 class CreateEventosTable extends Migration
 {
     /**
@@ -21,6 +23,46 @@ class CreateEventosTable extends Migration
             $table->string('end');
             $table->string('color');
         });
+
+        $data =  array(
+            [
+                'title' => 'Clase yoga',
+                'day' => '1',
+                'start' => '9:00',
+                'end' => '11:00',
+                'color' => 'fc-event-solid-danger'
+            ],
+            [
+                'title' => 'Clase crossfit',
+                'day' => '2',
+                'start' => '14:00',
+                'end' => '16:00',
+                'color' => 'fc-event-solid-warning'
+            ],
+            [
+                'title' => 'Clase pilates',
+                'day' => '5',
+                'start' => '17:00',
+                'end' => '20:00',
+                'color' => 'fc-event-solid-info'
+            ],
+            [
+                'title' => 'Clase aeróbicos',
+                'day' => '3',
+                'start' => '12:00',
+                'end' => '14:00',
+                'color' => 'fc-event-solid-primary'
+            ]
+        );
+        foreach ($data as $dato){
+            $evento = new Evento();
+            $evento->title = $dato['title'];
+            $evento->day = $dato['day'];
+            $evento->start = $dato['start'];
+            $evento->end = $dato['end'];
+            $evento->color = $dato['color'];
+            $evento->save();
+        }
     }
 
     /**
